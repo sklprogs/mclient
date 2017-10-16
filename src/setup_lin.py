ecc8258d8acf64dea3c675b3ba7877f6a51a3bc5
skl.progs <skl.progs@gmail.com>
2017-10-16 15:59:47 +0300

    Style
    Former-commit-id: 06869d54481312e95d45762a3140c73fb805ba7c

--- src/setup_lin.py
+++ src/setup_lin.py
@@ -2,14 +2,21 @@ from cx_Freeze import setup, Executable
 
 # Dependencies are automatically detected, but it might need
 # fine tuning.
-buildOptions = dict(packages = [], includes = ["re"], excludes = [])
+buildOptions = dict (packages = []
+                    ,includes = ["re"]
+                    ,excludes = []
+                    )
 
-executables = [
-    Executable('mclient.py', base = 'Console', icon = 'resources/icon_64x64_mclient.gif', targetName = 'mclient')
-]
+executables = [Executable ('mclient.py'
+                          ,base = 'Console'
+                          ,icon = 'resources/icon_64x64_mclient.gif'
+                          ,targetName = 'mclient'
+                          )
+              ]
 
-setup(name='mclient.py',
-      version = '5',
-      description = 'Multitran Online Client',
-      options = dict(build_exe = buildOptions),
-      executables = executables)
+setup (name        = 'mclient.py'
+      ,version     = '5'
+      ,description = 'Multitran Online Client'
+      ,options     = dict(build_exe=buildOptions)
+      ,executables = executables
+      )
