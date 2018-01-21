bc19f6d7c7c97906c8b8620e1862b5a0e67af33b
skl.progs <skl.progs@gmail.com>
2018-01-21 23:49:01 +0300

    Fix indentation
    Former-commit-id: bdeef7cab41554db18ebcb3460c4d102fa70878a

--- src/kl_mod_win.py
+++ src/kl_mod_win.py
@@ -15,140 +15,140 @@ import threading
 
 
 class KeyListener(threading.Thread):
-	
-	def __init__(self):
-		threading.Thread.__init__(self)
-		self.finished = threading.Event()
-		# Переменные должны быть инициализированы до вызова HookManager
-		self.pressed = []
-		self.listeners = {}
-		self.status = 0
-		''' Иногда по непонятной причине символы выходят в верхнем
+    
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.finished = threading.Event()
+        # Переменные должны быть инициализированы до вызова HookManager
+        self.pressed = []
+        self.listeners = {}
+        self.status = 0
+        ''' Иногда по непонятной причине символы выходят в верхнем
             регистре, но мы их приводим в нижний регистр в классе,
             поэтому здесь достаточно указать 'c'
         '''
-		self.addKeyListener ("Lcontrol+c+c"
+        self.addKeyListener ("Lcontrol+c+c"
                             ,lambda:keylistener.set_status(status=1)
                             )
-		self.addKeyListener ("Rcontrol+c+c"
+        self.addKeyListener ("Rcontrol+c+c"
                             ,lambda:keylistener.set_status(status=1)
                             )
-		self.addKeyListener ("Lcontrol+Insert+Insert"
+        self.addKeyListener ("Lcontrol+Insert+Insert"
                             ,lambda:keylistener.set_status(status=1)
                             )
-		self.addKeyListener ("Rcontrol+Insert+Insert"
+        self.addKeyListener ("Rcontrol+Insert+Insert"
                             ,lambda:keylistener.set_status(status=1)
                             )
-		self.addKeyListener ("Lmenu+Oem_3"
+        self.addKeyListener ("Lmenu+Oem_3"
                             ,lambda:keylistener.set_status(status=2)
                             )
-		self.addKeyListener ("Rmenu+Oem_3"
+        self.addKeyListener ("Rmenu+Oem_3"
                             ,lambda:keylistener.set_status(status=2)
                             )
-		self.restart()
+        self.restart()
 
-	def cancel(self):
-		self.hm.UnhookKeyboard()
-		self.finished.set()
+    def cancel(self):
+        self.hm.UnhookKeyboard()
+        self.finished.set()
 
-	def restart(self):
-		lock.acquire()
-		self.hm = HookManager()
-		self.hm.KeyDown = self.press
-		self.hm.KeyUp = self.release
-		self.hm.HookKeyboard()
-		lock.release()
+    def restart(self):
+        lock.acquire()
+        self.hm = HookManager()
+        self.hm.KeyDown = self.press
+        self.hm.KeyUp = self.release
+        self.hm.HookKeyboard()
+        lock.release()
 
-	def press(self,event):
-		character = str(event.Key)
-		if character:
-			if len(character) == 1:
-				character = character.lower()
-			print_v('Key released: %s' % str(character))
-			if len(self.pressed) == 2:
-				if self.pressed[1] == 'Oem_3':
-					self.pressed = []
-			elif len(self.pressed) == 3:
-				self.pressed = []
-			if character in ('Lcontrol','Rcontrol','Lmenu','Rmenu'):
-				self.pressed = [character]
-			elif character in ('c','C','Insert','Oem_3'):
-				if len(self.pressed) > 0:
-					if self.pressed[0] in ('Lcontrol','Rcontrol','Lmenu'
+    def press(self,event):
+        character = str(event.Key)
+        if character:
+            if len(character) == 1:
+                character = character.lower()
+            print_v('Key released: %s' % str(character))
+            if len(self.pressed) == 2:
+                if self.pressed[1] == 'Oem_3':
+                    self.pressed = []
+            elif len(self.pressed) == 3:
+                self.pressed = []
+            if character in ('Lcontrol','Rcontrol','Lmenu','Rmenu'):
+                self.pressed = [character]
+            elif character in ('c','C','Insert','Oem_3'):
+                if len(self.pressed) > 0:
+                    if self.pressed[0] in ('Lcontrol','Rcontrol','Lmenu'
                                           ,'Rmenu'
                                           ):
-						self.pressed.append(character)
-			action = self.listeners.get(tuple(self.pressed), False)
-			print_v('Current action: ' + str(tuple(self.pressed)))
-			if action:
-				action()
-		# Без этого получаем ошибку (an integer is required)
-		return True
+                        self.pressed.append(character)
+            action = self.listeners.get(tuple(self.pressed), False)
+            print_v('Current action: ' + str(tuple(self.pressed)))
+            if action:
+                action()
+        # Без этого получаем ошибку (an integer is required)
+        return True
 
-	def release(self,event):
-		"""must be called whenever a key release event has occurred."""
-		character = str(event.Key)
-		if character:
-			if len(character) == 1:
-				character = character.lower()
-			print_v('Key released: %s' % str(character))
-			# Не засчитывает отпущенный Control
-			# Кириллическую 'с' распознает как латинскую
-			if not character in ('c','C','Insert','Oem_3'):
-				self.pressed = []
-		# Без этого получаем ошибку (an integer is required)
-		return True
+    def release(self,event):
+        """must be called whenever a key release event has occurred."""
+        character = str(event.Key)
+        if character:
+            if len(character) == 1:
+                character = character.lower()
+            print_v('Key released: %s' % str(character))
+            # Не засчитывает отпущенный Control
+            # Кириллическую 'с' распознает как латинскую
+            if not character in ('c','C','Insert','Oem_3'):
+                self.pressed = []
+        # Без этого получаем ошибку (an integer is required)
+        return True
 
-	def addKeyListener(self, hotkeys, callable):
-		keys = tuple(hotkeys.split("+"))
-		print_v("Added new keylistener for :" + str(keys))
-		self.listeners[keys] = callable
-		
-	def set_status(self,status=0):
-		self.status = status
-		print_v('Setting status to %d!' % self.status)
-		
-	# Returns 0..2
+    def addKeyListener(self, hotkeys, callable):
+        keys = tuple(hotkeys.split("+"))
+        print_v("Added new keylistener for :" + str(keys))
+        self.listeners[keys] = callable
+        
+    def set_status(self,status=0):
+        self.status = status
+        print_v('Setting status to %d!' % self.status)
+        
+    # Returns 0..2
     def check(self):
-		if self.status:
-			print_v('Hotkey has been caught!')
-			status = self.status
-			self.status = 0
-			return status
+        if self.status:
+            print_v('Hotkey has been caught!')
+            status = self.status
+            self.status = 0
+            return status
 
 '''
-	Linux: Control_L, Control_R
-	Windows: Lcontrol, Rcontrol
+    Linux: Control_L, Control_R
+    Windows: Lcontrol, Rcontrol
 '''
 
 def print_v(*args):
-	if Verbose:
-		print(*args)
+    if Verbose:
+        print(*args)
 
 def wait_example():
-	from time import sleep
-	while not keylistener.check():
-		''' Нельзя делать одновременно pythoncom.PumpMessages() и
+    from time import sleep
+    while not keylistener.check():
+        ''' Нельзя делать одновременно pythoncom.PumpMessages() и
             pythoncom.PumpWaitingMessages() - они оба создают циклы
             Без этого result вообще почему-то не работает (видимо, здесь
             есть какой-то цикл, который необходим). Если же создать
             поток, он не сможет обнаружить flags['HotkeyCaught'].
         '''
-		pythoncom.PumpWaitingMessages()
-		''' Если поставить слишком большой интервал, например, 1, то
+        pythoncom.PumpWaitingMessages()
+        ''' Если поставить слишком большой интервал, например, 1, то
             вообще ничего не получим!
         '''
-		sleep(0.1)
-	keylistener.cancel()
-	
+        sleep(0.1)
+    keylistener.cancel()
+    
 def wait_cycle():
-	from time import sleep, time
-	print_v('You have 5 seconds only...')
-	timeout = time() + 5
-	while timeout > time():
-		pythoncom.PumpWaitingMessages()
-	keylistener.cancel()
-	
+    from time import sleep, time
+    print_v('You have 5 seconds only...')
+    timeout = time() + 5
+    while timeout > time():
+        pythoncom.PumpWaitingMessages()
+    keylistener.cancel()
+    
 lock = threading.Lock()
 Verbose = False
 keylistener = KeyListener()
@@ -156,4 +156,4 @@ keylistener = KeyListener()
 keylistener.start()
 
 if __name__ == '__main__':
-	wait_example()
+    wait_example()
