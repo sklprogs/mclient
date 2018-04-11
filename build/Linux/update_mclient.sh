45c2ddf28379e281d65b678870a8a5773f6f752e
skl.progs <skl.progs@gmail.com>
2018-04-12 02:48:25 +0300

    Linux build scripts
    Former-commit-id: 0ca6f936f5f5bce6012e78a9e9b7f34f4b5d59b1

--- build/Linux/update_mclient.sh
+++ build/Linux/update_mclient.sh
@@ -8,6 +8,7 @@ rmdir -p build/exe.linux-i686-3.4
 cp -ru ./resources ./user ./mclient/
 rm -r ./mclient/app/{libicudata.so.54,libicui18n.so.54,libicuuc.so.54,libQt*,platforms,imageformats}
 rm -r ./mclient/app/lib/python3.4/{dawg_python,enchant,importlib,lib2to3,PIL,pydoc_data,PyQt*,pymorphy2*}
+rm -r ./mclient/app/lib/python3.4/tkinterhtml/tkhtml/{MacOSX,Windows}
 ./pack.sh
 rm -rf ./mclient
 ./clean_up.sh
