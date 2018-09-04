46f48fab3dce31e79374a654e67196f29b5bb387
skl.progs <skl.progs@gmail.com>
2018-09-05 02:17:55 +0300

    Build scripts (Linux)
    Former-commit-id: 4025af7e6ca2a2f0fadc81e2d55b7608604c57dd

--- build/Linux/update_structure.sh
+++ build/Linux/update_structure.sh
@@ -2,7 +2,7 @@
 
 # Do not use "verbose" in order to spot errors easily
 
-mkdir -p ./{resources/{buttons,locale/ru/LC_MESSAGES},src/tkhtml,user/dics}
+mkdir -p ./{resources/{buttons,locale/ru/LC_MESSAGES},src,user/dics}
 
 # Copy mclient image resources
 cp -u /usr/local/bin/mclient/resources/buttons/icon_36x36_{alphabet_off,alphabet_on,block_off,block_on,clear_search_field,define,go_back,go_back_off,go_forward,go_forward_off,go_search,open_in_browser,paste,print,priority_off,priority_on,quit_now,reload,repeat_sign2,repeat_sign2_off,repeat_sign,repeat_sign_off,save_article,search_article,settings,show_about,spec_symbol,toggle_history,toggle_view_hor,toggle_view_ver,watch_clipboard_off,watch_clipboard_on}.gif ./resources/buttons/
@@ -28,7 +28,7 @@ cp -u /usr/local/bin/mclient/user/dics/rm_dics.sh ./user/dics/
 # (All platforms) Copy mclient icon
 cp -u /usr/local/bin/mclient/resources/icon_64x64_mclient.gif ./resources/
 
-# (Linux-only) Copy tkinterhtml (follows the symlink)
-cp -r /usr/local/bin/tkhtml/* ./src/tkhtml/
+# (Linux-only) Copy tkinterhtml
+cp -r /usr/lib/python3.4/site-packages/tkinterhtml ./src/
 
 ls --color=always .
