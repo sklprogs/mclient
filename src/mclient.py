2f0dc8873306145e2c9165beda319609aaf9cd42
skl.progs <skl.progs@gmail.com>
2022-02-04 23:42:59 +0300

    Fix for non-Windows systems

--- src/mclient.py
+++ src/mclient.py
@@ -852,7 +852,8 @@ def run_timed_update():
                 objs.get_webframe().go_search()
         if check == 2 or sh.lg.globs['bool']['CaptureHotkey']:
             call_app()
-        kl.keylistener.restart()
+        if sh.objs.get_os().is_win():
+            kl.keylistener.restart()
     sh.objs.get_root().widget.after(2000,run_timed_update)
 
 
