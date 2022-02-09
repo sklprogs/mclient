62b40b50f6018b809ce4e45c0b078a6e47617056
skl.progs <skl.progs@gmail.com>
2022-02-10 01:57:12 +0300

    Update version; CHANGELOG

--- src/about/gui.py
+++ src/about/gui.py
@@ -4,7 +4,7 @@
 from skl_shared.localize import _
 import skl_shared.shared as sh
 
-VERSION = '6.13.1'
+VERSION = '6.13.2'
 CURYEAR = 2022
 ICON = sh.objs.get_pdir().add('..','resources','icon_64x64_mclient.gif')
 
