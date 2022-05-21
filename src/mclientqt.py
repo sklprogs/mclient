5b369cc4cee1f6e05f2f1b93595b2c88de85b754
skl.progs <skl.progs@gmail.com>
2022-05-21 20:51:56 +0300

    Add stub opt_col, opt_lg1, opt_lg2

--- src/mclientqt.py
+++ src/mclientqt.py
@@ -151,6 +151,11 @@ class App:
                   )
         self.gui.panel.opt_src.reset(sources)
         self.gui.panel.opt_col.reset((1,2,3,4,5,6,7,8,9,10),4)
+        #TODO: load from logic
+        langs1 = (_('English'),_('Russian'),_('French'))
+        langs2 = (_('Russian'),_('English'),_('French'))
+        self.gui.panel.opt_lg1.reset(langs1)
+        self.gui.panel.opt_lg2.reset(langs2)
     
     def show(self,event=None):
         self.gui.show()
