9f0ef92d4351e7361775756773e2465047ab42c4
skl.progs <skl.progs@gmail.com>
2022-08-25 22:52:24 +0300

    Add custom delegate

--- src/gui.py
+++ src/gui.py
@@ -8,6 +8,37 @@ from skl_shared_qt.localize import _
 import skl_shared_qt.shared as sh
 
 
+class CustomDelegate(PyQt5.QtWidgets.QStyledItemDelegate):
+    # akej74, https://stackoverflow.com/questions/35397943/how-to-make-a-fast-qtableview-with-html-formatted-and-clickable-cells
+    def paint(self,painter,option,index):
+        options = PyQt5.QtWidgets.QStyleOptionViewItem(option)
+        self.initStyleOption(options,index)
+    
+        if options.widget:
+            style = options.widget.style()
+        else:
+            style = PyQt5.QtWidgets.QApplication.style()
+    
+        doc = PyQt5.QtGui.QTextDocument()
+        doc.setHtml(options.text)
+        options.text = ''
+    
+        style.drawControl(PyQt5.QtWidgets.QStyle.CE_ItemViewItem,options,painter)
+        ctx = PyQt5.QtGui.QAbstractTextDocumentLayout.PaintContext()
+    
+        textRect = style.subElementRect(PyQt5.QtWidgets.QStyle.SE_ItemViewItemText,options)
+    
+        painter.save()
+    
+        painter.translate(textRect.topLeft())
+        painter.setClipRect(textRect.translated(-textRect.topLeft()))
+        painter.translate(0,0.5*(options.rect.height() - doc.size().height()))
+        doc.documentLayout().draw(painter,ctx)
+    
+        painter.restore()
+
+
+
 class Table(PyQt5.QtWidgets.QWidget):
     
     def __init__(self,*args,**kwargs):
@@ -60,6 +91,7 @@ class Table(PyQt5.QtWidgets.QWidget):
         #self.layout.setContentsMargins(0,0,0,0)
         #self.setLayout(self.layout)
         self.table = PyQt5.QtWidgets.QTableWidget(self)
+        self.table.setItemDelegate(CustomDelegate())
         self.hheader = self.table.horizontalHeader()
         self.vheader = self.table.verticalHeader()
         # This is required to activate mouse hovering
