4295ef7be6167a1e5a67c6021314e59a7039de80
skl.progs <skl.progs@gmail.com>
2022-10-06 01:52:19 +0300

    Update version; TODOS

--- src/about/gui.py
+++ src/about/gui.py
@@ -4,7 +4,7 @@
 from skl_shared.localize import _
 import skl_shared.shared as sh
 
-VERSION = '6.14.2'
+VERSION = '6.14.3'
 CURYEAR = 2022
 ICON = sh.objs.get_pdir().add('..','resources','icon_64x64_mclient.gif')
 
