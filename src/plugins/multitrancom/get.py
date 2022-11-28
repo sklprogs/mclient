f90ad93f67aba98149fa1bc3c7130481e15e9c08
skl.progs <skl.progs@gmail.com>
2022-11-28 05:18:49 +0300

    Refactor plugins.multitrancom.get

--- src/plugins/multitrancom/get.py
+++ src/plugins/multitrancom/get.py
@@ -67,43 +67,37 @@ class Suggest:
     
     def get_url(self):
         f = '[MClient] plugins.multitrancom.get.Suggest.get_url'
-        if self.Success:
-            ''' #NOTE: the encoding here MUST be 'utf-8' irrespective
-                of the plugin!
-            '''
-            self.url = sh.Online (base = self.pair
-                                 ,pattern = self.pattern
-                                 ,coding = 'utf-8'
-                                 ).get_url()
-            if self.url:
-                if not '&SHL=' in self.url:
-                    self.url += EXT
-            else:
-                sh.com.rep_empty(f)
-                self.Success = False
-        else:
+        if not self.Success:
             sh.com.cancel(f)
+            return
+        #NOTE: The encoding here MUST be 'utf-8' irrespective of the plugin!
+        self.url = sh.Online (base = self.pair
+                             ,pattern = self.pattern
+                             ,coding = 'utf-8'
+                             ).get_url()
+        if not self.url:
+            sh.com.rep_empty(f)
+            self.Success = False
+            return
+        if not '&SHL=' in self.url:
+            self.url += EXT
     
     def get(self):
         f = '[MClient] plugins.multitrancom.get.Suggest.get'
-        if self.Success:
-            ''' #NOTE: the encoding here (unlike 'self.url')
-                is plugin-dependent.
-            '''
-            self.items = sh.Get (url = self.url
-                                ,coding = CODING
-                                ).run()
-            if self.items:
-                self.items = html.unescape(self.items)
-                self.items = [item for item \
-                              in self.items.splitlines() if item
-                             ]
-                sh.objs.get_mes(f,self.items,True).show_debug()
-                return self.items
-            else:
-                sh.com.rep_empty(f)
-        else:
+        if not self.Success:
             sh.com.cancel(f)
+            return
+        # #NOTE: the encoding here (unlike 'self.url') is plugin-dependent
+        self.items = sh.Get (url = self.url
+                            ,coding = CODING
+                            ).run()
+        if not self.items:
+            sh.com.rep_empty(f)
+            return
+        self.items = html.unescape(self.items)
+        self.items = [item for item in self.items.splitlines() if item]
+        sh.objs.get_mes(f,self.items,True).show_debug()
+        return self.items
     
     def run(self):
         self.get_url()
@@ -134,58 +128,52 @@ class Get:
         
     def decode(self):
         f = '[MClient] plugins.multitrancom.get.Get.decode'
-        if self.Success:
-            ''' If the page is not loaded, we obviously cannot change
-                its encoding.
-            '''
-            if self.text:
-                try:
-                    self.htm = self.text = self.text.decode(CODING)
-                except Exception as e:
-                    self.Success = False
-                    self.htm = ''
-                    self.text = ''
-                    mes = _('Unable to change the web-page encoding!\n\nDetails: {}')
-                    mes = mes.format(e)
-                    sh.objs.get_mes(f,mes).show_error()
-            else:
-                sh.com.rep_empty(f)
-        else:
+        if not self.Success:
             sh.com.cancel(f)
+            return
+        # If the page is not loaded, we obviously cannot change its encoding
+        if not self.text:
+            sh.com.rep_empty(f)
+            return
+        try:
+            self.htm = self.text = self.text.decode(CODING)
+        except Exception as e:
+            self.Success = False
+            self.htm = ''
+            self.text = ''
+            mes = _('Unable to change the web-page encoding!\n\nDetails: {}')
+            mes = mes.format(e)
+            sh.objs.get_mes(f,mes).show_error()
     
     def get(self):
         f = '[MClient] plugins.multitrancom.get.Get.get'
-        if self.Success:
-            while not self.text:
-                try:
-                    mes = _('Get online: "{}"').format(self.pattern)
-                    sh.objs.get_mes(f,mes,True).show_info()
-                    ''' - If the page is loaded using
-                          "page=urllib.request.urlopen(my_url)", we get
-                          HTTPResponse as a result, which is useful only
-                          to remove JavaScript tags. Thus, if we remove
-                          all excessive tags manually, then we need
-                          a string as output.
-                        - If 'self.url' is empty, then an error is
-                          thrown.
-                    '''
-                    self.text = urllib.request.urlopen (self.url
-                                                       ,None
-                                                       ,TIMEOUT
-                                                       ).read()
-                    mes = _('[OK]: "{}"').format(self.pattern)
-                    sh.objs.get_mes(f,mes,True).show_info()
-                # Too many possible exceptions
-                except Exception as e:
-                    mes = _('[FAILED]: "{}"').format(self.pattern)
-                    sh.objs.get_mes(f,mes,True).show_error()
-                    # For some reason, 'break' does not work here
-                    mes = _('Unable to get the webpage. Do you want to try again?\n\nDetails: {}')
-                    mes = mes.format(e)
-                    if not sh.objs.get_mes(f,mes).show_question():
-                        return
-        else:
+        if not self.Success:
             sh.com.cancel(f)
+            return
+        while not self.text:
+            try:
+                mes = _('Get online: "{}"').format(self.pattern)
+                sh.objs.get_mes(f,mes,True).show_info()
+                ''' - If the page is loaded using
+                      "page=urllib.request.urlopen(my_url)", we get
+                      HTTPResponse as a result, which is useful only to remove
+                      JavaScript tags. Thus, if we remove all excessive tags
+                      manually, then we need a string as output.
+                    - If 'self.url' is empty, then an error is thrown.
+                '''
+                self.text = urllib.request.urlopen (self.url,None,TIMEOUT
+                                                   ).read()
+                mes = _('[OK]: "{}"').format(self.pattern)
+                sh.objs.get_mes(f,mes,True).show_info()
+            # Too many possible exceptions
+            except Exception as e:
+                mes = _('[FAILED]: "{}"').format(self.pattern)
+                sh.objs.get_mes(f,mes,True).show_error()
+                # For some reason, 'break' does not work here
+                mes = _('Unable to get the webpage. Do you want to try again?\n\nDetails: {}')
+                mes = mes.format(e)
+                if not sh.objs.get_mes(f,mes).show_question():
+                    return
 
 
 
@@ -201,37 +189,36 @@ class Commands:
     
     def get_url(self,code1,code2,search):
         f = '[MClient] plugins.multitrancom.get.Commands.get_url'
-        if search and code1 and code2:
-            #NOTE: The encoding here should always be 'utf-8'!
-            base = 'https://www.multitran.com/m.exe?s=%s&l1={}&l2={}'
-            base = base.format(code1,code2)
-            url = sh.Online (base = base
-                            ,pattern = search
-                            ,coding = 'utf-8'
-                            ).get_url()
-            if url and not '&SHL=' in url:
-                url += EXT
-            return url
-        else:
+        if not search or not code1 or not code2:
             sh.com.rep_empty(f)
+            return
+        #NOTE: The encoding here should always be 'utf-8'!
+        base = 'https://www.multitran.com/m.exe?s=%s&l1={}&l2={}'
+        base = base.format(code1,code2)
+        url = sh.Online (base = base
+                        ,pattern = search
+                        ,coding = 'utf-8'
+                        ).get_url()
+        if url and not '&SHL=' in url:
+            url += EXT
+        return url
     
     def fix_url(self,url):
         f = '[MClient] plugins.multitrancom.get.Commands.fix_url'
-        if url:
-            try:
-                ind = url.index('" title')
-                url = url[:ind]
-            except ValueError:
-                pass
-            url = w3lib.url.safe_url_string(url)
-            if not url.startswith('http'):
-                url = PAIRROOT + url
-            if not '&SHL=' in url:
-                url += EXT
-            return url
-        else:
+        if not url:
             sh.com.rep_empty(f)
             return ''
+        try:
+            ind = url.index('" title')
+            url = url[:ind]
+        except ValueError:
+            pass
+        url = w3lib.url.safe_url_string(url)
+        if not url.startswith('http'):
+            url = PAIRROOT + url
+        if not '&SHL=' in url:
+            url += EXT
+        return url
 
     def is_accessible(self):
         try:
