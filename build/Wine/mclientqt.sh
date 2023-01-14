11ae7a923f28ed85eb6c84b2685177bc27b1a8aa
skl.progs <skl.progs@gmail.com>
2023-01-14 03:04:32 +0300

    Update Wine build script

--- build/Wine/mclientqt.sh
+++ build/Wine/mclientqt.sh
@@ -8,13 +8,19 @@ shared="skl_shared_qt"
 binariesdir="$HOME/binaries"
 bindir="$HOME/bin"
 productdir="$bindir/$product"
+resdir="$bindir/$product/resources"
 shareddir="$bindir/$shared"
+cmd="$productdir/build/Wine/$product.cmd"
 producttmp="$WINEPREFIX/drive_c/$product" # Will be deleted!
 sharedtmp="$WINEPREFIX/drive_c/$shared"   # Will be deleted!
 buildtmp="$producttmp/$product"           # Will be deleted!
 
 if [ ! -e "$pyinstaller" ]; then
     echo "pyinstaller is not installed!"; exit
+fi
+
+if [ ! -e "$cmd" ]; then
+    echo "File $cmd does not exist!"; exit
 fi
 
 if [ ! -d "$binariesdir/$product" ]; then
@@ -25,6 +31,10 @@ if [ ! -d "$productdir" ]; then
     echo "Folder $productdir does not exist!"; exit
 fi
 
+if [ ! -d "$resdir" ]; then
+    echo "Folder $resdir does not exist!"; exit
+fi
+
 if [ ! -d "$shareddir" ]; then
     echo "Folder $shareddir does not exist!"; exit
 fi
@@ -32,14 +42,15 @@ fi
 rsync -aL --delete-before --exclude='.git' "$productdir/" "$producttmp"
 rsync -aL --delete-before --exclude='.git' "$shareddir/" "$sharedtmp"
 
-cd "$producttmp"
+mkdir "$buildtmp"
+
+cd "$producttmp"/src
 # Icon path should be Windows-compliant. Only ICO and EXE formats are supported.
-wine "$pyinstaller" -w -i ./resources/$product.ico "src/$product.py"
+wine "$pyinstaller" -w -i ../resources/$product.ico "$product.py"
 
-mv "$producttmp/build/$product" "$buildtmp"
-
-cp -r "$productdir/resources" "$buildtmp"
-mv "$producttmp/dist/$product" "$buildtmp/app"
+mv "$producttmp/src/dist/$product" "$buildtmp/app"
+cp -r "$resdir" "$buildtmp"/
+cp "$cmd" "$buildtmp"
 
 # Tesh launch
 cd "$buildtmp/app"
