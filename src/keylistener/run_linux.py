75449d943af9894d039df00f42ed6a0e940992aa
skl.progs <skl.progs@gmail.com>
2023-03-22 02:34:44 +0300

    keylistener: Add abstraction layers [unstable]

--- src/keylistener/run_linux.py
+++ src/keylistener/run_linux.py
@@ -3,8 +3,9 @@
 
 import sys
 import time
-import PyQt5.QtCore
+
 import PyQt5.QtWidgets
+import PyQt5.QtCore
 
 from skl_shared_qt.localize import _
 import skl_shared_qt.shared as sh
@@ -17,28 +18,79 @@ else:
     #TODO: create and import dummy module
     import linux as osid
 
+import gui as gi
 
-class Catcher(PyQt5.QtCore.QObject):
+
+class Catcher:
     
-    sig_catch = PyQt5.QtCore.pyqtSignal(int)
-    sig_end = PyQt5.QtCore.pyqtSignal()
-    
-    def __init__(self,*args,**kwargs):
-        super().__init__(*args,**kwargs)
+    def __init__(self):
         self.Running = True
+        self.gui = gi.Catcher()
     
     def run(self):
         while self.Running:
             # 'osid.keylistener.status' is reset to 0 after catching a hotkey
             status = osid.keylistener.check()
             if status:
-                self.sig_catch.emit(status)
+                self.gui.catch(status)
             time.sleep(.5)
     
-    def cancel(self):
+    def end(self):
         osid.keylistener.cancel()
         self.Running = False
-        self.sig_end.emit()
+        self.gui.end()
+    
+    def move_to_thread(self,thread):
+        self.gui.move_to_thread(thread)
+    
+    def bind_catch(self,action):
+        self.gui.bind_catch(action)
+    
+    def bind_end(self,action):
+        self.gui.bind_end(action)
+    
+    def delete_later(self):
+        self.gui.delete_later()
+
+
+
+class Thread:
+    
+    def __init__(self,catch_action):
+        self.catcher = Catcher()
+        self.thread = gi.Thread()
+        self.bind(catch_action)
+    
+    def delete_later(self):
+        self.catcher.delete_later()
+        self.thread.delete_later()
+    
+    def _bind_start(self):
+        self.thread.bind_start(self.catcher.run)
+    
+    def _bind_end(self):
+        self.catcher.bind_end(self.thread.quit)
+        self.catcher.bind_end(self.delete_later)
+    
+    def _bind_catch(self,catch_action):
+        self.catcher.bind_catch(catch_action)
+    
+    def bind(self,catch_action):
+        self._bind_start()
+        self._bind_catch(catch_action)
+        self._bind_end()
+    
+    def start(self):
+        self.catcher.move_to_thread(self.thread)
+        self.thread.start()
+    
+    def wait(self):
+        # Calling in-built function
+        self.gui.wait()
+    
+    def end(self):
+        self.catcher.end()
+        self.wait()
 
 
 
@@ -46,11 +98,11 @@ class App(PyQt5.QtWidgets.QWidget):
     
     def __init__(self,*args,**kwargs):
         super().__init__(*args,**kwargs)
+        self.set_thread()
         self.set_gui()
     
     def closeEvent(self,event):
-        self.catcher.cancel()
-        self.thread.wait()
+        self.thread.end()
         return super().closeEvent(event)
     
     def bind(self,hotkey,action):
@@ -67,22 +119,18 @@ class App(PyQt5.QtWidgets.QWidget):
         layout_.addWidget(self.button)
         self.setLayout(layout_)
     
-    def run_thread(self):
-        self.thread = PyQt5.QtCore.QThread()
-        self.catcher = Catcher()
-        self.catcher.moveToThread(self.thread)
-        self.thread.started.connect(self.catcher.run)
-        self.catcher.sig_catch.connect(self.report)
-        self.catcher.sig_end.connect(self.thread.quit)
-        self.catcher.sig_end.connect(self.catcher.deleteLater)
-        self.catcher.sig_end.connect(self.thread.deleteLater)
+    def set_thread(self):
+        self.thread = Thread(self.report)
         self.thread.start()
+        
 
 
 if __name__ == '__main__':
     f = '__main__'
-    exe = PyQt5.QtWidgets.QApplication(sys.argv)
+    sh.com.start()
+    #exe = PyQt5.QtWidgets.QApplication(sys.argv)
     app = App()
+    #app.set_thread()
     app.show()
-    app.run_thread()
-    sys.exit(exe.exec_())
+    #sys.exit(exe.exec_())
+    sh.com.end()
