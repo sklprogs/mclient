2a91ec82d628b5e0ae6bce2d5908b7c61ea43cff
skl.progs <skl.progs@gmail.com>
2023-06-26 01:35:31 +0300

    plugins.stardict.cleanup: Add spaces after commas

--- src/plugins/stardict/cleanup.py
+++ src/plugins/stardict/cleanup.py
@@ -8,8 +8,8 @@ import skl_shared_qt.shared as sh
 
 
 #TODO (?): use shared
-speech_abbr = ('гл.','нареч.','прил.','сокр.','сущ.')
-dic_abbr = ('вчт.','карт.','мор.','разг.','уст.','хир.','эл.')
+speech_abbr = ('гл.', 'нареч.', 'прил.', 'сокр.', 'сущ.')
+dic_abbr = ('вчт.', 'карт.', 'мор.', 'разг.', 'уст.', 'хир.', 'эл.')
 #TODO: read from file
 dic_titles = ('(австралийское)'
              ,'(американизм)'
@@ -34,26 +34,26 @@ header = '~'
 
 class Common:
     
-    def __init__(self,text):
+    def __init__(self, text):
         self.text = text
     
     def decode_entities(self):
-        ''' Needed both for MT and Stardict. Convert HTM entities
-            to a human readable format, e.g., '&copy;' -> '©'.
+        ''' Needed both for MT and Stardict. Convert HTM entities to a human
+            readable format, e.g., '&copy;' -> '©'.
         '''
         f = '[MClient] plugins.stardict.cleanup.Common.decode_entities'
         try:
             self.text = html.unescape(self.text)
         except Exception as e:
-            sh.com.rep_failed(f,e)
+            sh.com.rep_failed(f, e)
     
     def delete_trash(self):
-        self.text = self.text.replace('\r\n','')
-        self.text = self.text.replace('\n','')
-        self.text = self.text.replace('\xa0',' ')
+        self.text = self.text.replace('\r\n', '')
+        self.text = self.text.replace('\n', '')
+        self.text = self.text.replace('\xa0', ' ')
         while '  ' in self.text:
-            self.text = self.text.replace('  ',' ')
-        self.text = re.sub(r'\>[\s]{0,1}\<','\><',self.text)
+            self.text = self.text.replace('  ', ' ')
+        self.text = re.sub(r'\>[\s]{0,1}\<', '\><', self.text)
     
     def run(self):
         f = '[MClient] plugins.stardict.cleanup.Common.run'
@@ -72,7 +72,7 @@ class Type1:
     ''' Formatting example:
         <dic>dicEnRu</dic><k>cut</k><tr>kʌt</tr> I 1. гл. 1) резать, разрезать I cut my arm. ≈ Я порезал руку. Cut the bread. ≈ Разрежьте хлеб. Syn : slash, lance, slit; slice 2) а) завершать, прекращать; кончать Cut the rap. ≈ Хватит болтать. б) жать, косить ∙ Syn : mow, prune
     '''
-    def __init__(self,text):
+    def __init__(self, text):
         self.blocks = []
         self.tags = []
         self.text = text
@@ -83,26 +83,26 @@ class Type1:
         try:
             self.text = html.unescape(self.text)
         except Exception as e:
-            sh.com.rep_failed(f,e)
+            sh.com.rep_failed(f, e)
     
     def delete_trash(self):
-        self.text = self.text.replace(' ∙ ',';').replace(' - ',';')
+        self.text = self.text.replace(' ∙ ', ';').replace(' - ', ';')
         #TODO: Can we allow decoding before extracting tags?
-        self.text = self.text.replace('&gt;','')
+        self.text = self.text.replace('&gt;', '')
         
     def delete_numbering(self):
-        self.text = re.sub(' \d+[\)\.]',';',self.text)
+        self.text = re.sub(' \d+[\)\.]', ';', self.text)
         
     def delete_alpha_numbering(self):
-        self.text = re.sub(' [а-я]\)',';',self.text)
-        self.text = re.sub(' [a-z]\)',';',self.text)
+        self.text = re.sub(' [а-я]\)', ';', self.text)
+        self.text = re.sub(' [a-z]\)', ';', self.text)
         
     def delete_roman_numbering(self):
-        self.text = re.sub('> I*','>',self.text)
-        self.text = self.text.replace('II','').replace('III','').replace(' IV ','').replace(' V ','')
+        self.text = re.sub('> I*', '>', self.text)
+        self.text = self.text.replace('II', '').replace('III', '').replace(' IV ', '').replace(' V ', '')
         
     def restore_header(self):
-        self.text = self.text.replace('*',header).replace('~',header)
+        self.text = self.text.replace('*', header).replace('~', header)
     
     def run(self):
         self.delete_trash()
@@ -171,11 +171,11 @@ class Type1:
             else:
                 block += sym
         self.blocks = [block.strip() for block in self.blocks \
-                        if block.strip()
-                       ]
+                       if block.strip()
+                      ]
         self.blocks = [block.strip(',') for block in self.blocks \
-                        if block.strip(',')
-                       ]
+                       if block.strip(',')
+                      ]
         return self.blocks
 
     def get_tags(self):
@@ -189,8 +189,8 @@ class Type1:
                 self.tags.append('<dic>' + block + '</dic>')
             elif block in dic_titles:
                 self.tags.append('<dic>' + block + '</dic>')
-                ''' #TODO: create a 'Synonyms' subject, split items
-                    after it and set 'term' type to them
+                ''' #TODO: create a 'Synonyms' subject, split items after it
+                    and set 'term' type to them.
                 '''
             elif block.startswith('(') or '≈' in block or 'Syn :' \
             in block:
@@ -221,7 +221,7 @@ class Type2:
     ''' Formatting example:
         <dic>dicEnRu</dic>1> порез; надрез; _Ex: I cut my arm _общ. Я порезал руку2> короткий путь3> _мат. раздел; _Ex: please refer to this cut обратитесь к этому разделу _Ex: to cut into pieces рассечь на части
     '''
-    def __init__(self,text):
+    def __init__(self, text):
         self.blocks = []
         self.tags = []
         self.text = text
@@ -262,9 +262,9 @@ class Type2:
                         self.blocks.append(block)
                         block = self.text[i]
             elif self.text[i] == '.':
-                ''' Split text only when a short dictionary title is
-                    met, ignore the dot otherwise (especially useful
-                    when "smb.", "smb.'s", "smth.", "т.п." are met).
+                ''' Split text only when a short dictionary title is met,
+                    ignore the dot otherwise (especially useful when "smb.",
+                    "smb.'s", "smth.", "т.п." are met).
                 '''
                 block += self.text[i]
                 if self.is_dic(block):
@@ -281,11 +281,11 @@ class Type2:
         return self.blocks
     
     def delete_numbering(self):
-        self.text = re.sub('\d+\> ',';',self.text)
+        self.text = re.sub('\d+\> ', ';',self.text)
     
     def restore_header(self):
         #TODO: are there are other types of headers?
-        self.text = self.text.replace('~',header)
+        self.text = self.text.replace('~', header)
     
     #TODO: do this before anything else
     def decode(self):
@@ -293,12 +293,12 @@ class Type2:
         try:
             self.text = html.unescape(self.text)
         except Exception as e:
-            sh.com.rep_failed(f,e)
+            sh.com.rep_failed(f, e)
     
     def delete_trash(self):
-        self.text = self.text.replace(' - ',';')
+        self.text = self.text.replace(' - ', ';')
         #TODO: Can we allow decoding before extracting tags?
-        self.text = self.text.replace('&gt;','')
+        self.text = self.text.replace('&gt;', '')
     
     def strip(self):
         ''' Stripping blocks can be necessary after splitting ';'.
@@ -329,14 +329,13 @@ class Type2:
                     expand short dictionary titles later.
                 '''
             elif self.is_dic(block):
-                block = block.replace('_','',1)
+                block = block.replace('_', '', 1)
                 self.tags.append('<dic>' + block + '</dic>')
             elif block.startswith('_Ex:'):
-                block = block.replace('_Ex:','',1)
-                ''' Examples are essentially comments, but we draft
-                    an original as a term because we have both 
-                    an original and a translation, and 
-                    the translation is parsed as a term.
+                block = block.replace('_Ex:', '', 1)
+                ''' Examples are essentially comments, but we draft an original
+                    as a term because we have both  an original and
+                    a translation, and the translation is parsed as a term.
                 '''
                 self.tags.append('<dtrn>' + block + '</dtrn>')
             elif block.startswith('_Id: '):
@@ -350,7 +349,7 @@ class Type2:
                 self.tags.append('<dtrn>' + block + '</dtrn>')
         return self.tags
 
-    def is_dic(self,string):
+    def is_dic(self, string):
         if string.startswith('_') and string == string.lower():
             return True
     
@@ -375,13 +374,13 @@ class Type3:
     ''' Formatting example:
         <dic>dicEnRu</dic><k>цель</k><b>I</b><dtrn>aim</dtrn><b>II</b><c><co>при стрельбе</co></c><dtrn>target</dtrn>
     '''
-    def __init__(self,text):
+    def __init__(self, text):
         self.text = text
         
     def delete_disamb(self):
         # This is done to speed up and eliminate tag disambiguation
         try:
-            self.text = self.text.replace('<i>','').replace('</i>','')
+            self.text = self.text.replace('<i>', '').replace('</i>', '')
         # Encoding has failed
         except TypeError:
             self.text = ''
@@ -394,7 +393,7 @@ class Type3:
 
 class CleanUp:
     # This class is basically needed for compliance with other code
-    def __init__(self,text):
+    def __init__(self, text):
         self.text = text
         if self.text is None:
             self.text = ''
@@ -406,15 +405,15 @@ class CleanUp:
             self.text = Common(self.text).run()
             if '<dtrn>' in self.text:
                 mes = _('Type 3')
-                sh.objs.get_mes(f,mes,True).show_debug()
+                sh.objs.get_mes(f, mes, True).show_debug()
                 self.text = Type3(self.text).run()
-            elif '_Ex:' in self.text or re.search('\d\>',self.text):
+            elif '_Ex:' in self.text or re.search('\d\>', self.text):
                 mes = _('Type 2')
-                sh.objs.get_mes(f,mes,True).show_debug()
+                sh.objs.get_mes(f, mes, True).show_debug()
                 self.text = Type2(self.text).run()
             else:
                 mes = _('Type 1')
-                sh.objs.get_mes(f,mes,True).show_debug()
+                sh.objs.get_mes(f, mes, True).show_debug()
                 self.text = Type1(self.text).run()
         else:
             sh.com.rep_empty(f)
