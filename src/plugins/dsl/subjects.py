fbd5e2486c7f8517f0d846d5720b576269ea9ea7
skl.progs <skl.progs@gmail.com>
2023-06-25 13:29:12 +0300

    plugins.dsl.subjects: Add spaces after commas

--- src/plugins/dsl/subjects.py
+++ src/plugins/dsl/subjects.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # -*- coding: UTF-8 -*-
 
-from skl_shared_qt.localize import _
+#from skl_shared_qt.localize import _
 import skl_shared_qt.shared as sh
 
 
@@ -20,10 +20,10 @@ class Subjects:
     def get_list(self):
         return self.lst
     
-    def get_group(self,subject):
+    def get_group(self, subject):
         return []
     
-    def get_group_with_header(self,subject):
+    def get_group_with_header(self, subject):
         return [subject]
 
 
@@ -48,6 +48,6 @@ if __name__ == '__main__':
     timer = sh.Timer(f)
     timer.start()
     mes = Subjects().get_group('Wood processing')
-    sh.objs.get_mes(f,mes,True).show_debug()
+    sh.objs.get_mes(f, mes, True).show_debug()
     timer.end()
     sh.com.end()
