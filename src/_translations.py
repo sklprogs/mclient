6cca2407c86d6ad8cf0eff38286e8914f776ce1a
skl.progs <skl.progs@gmail.com>
2023-07-26 04:12:37 +0300

    Update localization

---
+++ src/_translations.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+
+from skl_shared_qt.localize import _
+
+''' These are translations that Poedit considers outdated and tries to remove
+    since they are either stored in non-Python files (e.g. in JSON) or Poedit
+    just fails to extract them (e.g. f'<h2>{_("Main hotkeys")}</h2>'). We keep
+    them here just to be able to continue using Poedit.
+'''
+
+_("Clear history")
+_("Go to the next section of column #1")
+_("Go to the previous section of column #1")
+_("Go to the next section of column #2")
+_("Go to the previous section of column #2")
+_("Go to the next section of column #3")
+_("Go to the previous section of column #3")
+_("Go to the next section of column #4")
+_("Go to the previous section of column #4")
+_("Copy the URL of the current article")
+_("Copy the nominative case")
+_("Copy the current selection")
+_("Copy the URL of the selected cell")
+_("Open a webpage with a definition of the current term")
+_("Go to the preceding article")
+_("Go to the following article")
+_("Look up phrases")
+_("Next source language")
+_("Next target language")
+_("Open the current article in a default browser")
+_("Previous source language")
+_("Previous target language")
+_("Create a printer-friendly page")
+_("Quit the program")
+_("Start a new search in the current article")
+_("Reload the current article")
+_("Save or copy the current article")
+_("Search the article backward")
+_("Search the article forward")
+_("Swap source and target languages")
+_("About the program")
+_("Toggle alphabetizing")
+_("Toggle blacklisting")
+_("Toggle history")
+_("Toggle full cell text")
+_("Toggle prioritizing")
+_("Show settings")
+_("Paste a special symbol")
+_("Main hotkeys")
