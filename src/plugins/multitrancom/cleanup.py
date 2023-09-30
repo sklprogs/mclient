8d7e2055a0475e09a9904cf96b2216b2d1dc7b7b
skl.progs <skl.progs@gmail.com>
2023-09-30 15:02:00 +0300

    plugins.multitrancom: Fix parts of speech (new change on site)

--- src/plugins/multitrancom/cleanup.py
+++ src/plugins/multitrancom/cleanup.py
@@ -22,6 +22,9 @@ class CleanUp:
         self.text = self.text.replace('</u>','')
         self.text = self.text.replace('<b>','')
         self.text = self.text.replace('</b>','')
+        # Fix parts of speech
+        self.text = self.text.replace('<em><span style="color:gray">', '<em>')
+        self.text = self.text.replace('</span></em>', '</em>')
     
     def fix_href(self):
         ''' Fix a malformed URL, e.g., 'href="/m.exe?a=110&l1=1&l2=2&s=process (<редк.>)&sc=671"'
