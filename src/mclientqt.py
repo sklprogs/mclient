5079c152d62bb03408310df98f7ff78ed6bd857a
skl.progs <skl.progs@gmail.com>
2023-12-05 22:07:51 +0300

    Fix popup

--- src/mclientqt.py
+++ src/mclientqt.py
@@ -658,11 +658,11 @@ class Table:
             sh.com.rep_empty(f)
             return
         rowno, colno = self.get_cell()
-        max_width = objs.get_app().gui.article.get_width()
+        max_width = objs.get_app().get_width()
         width = self.gui.get_col_width(colno)
         height = self.gui.get_row_height(rowno)
-        win_y = objs.app.gui.article.get_y()
-        x1 = self.gui.get_cell_x(colno) + objs.app.gui.article.get_x()
+        win_y = objs.app.gui.get_y()
+        x1 = self.gui.get_cell_x(colno) + objs.app.get_x()
         if cf.objs.config.new['popup']['center']:
             y1 = self.gui.get_cell_y(rowno) + win_y - height / 2
             if y1 < win_y:
