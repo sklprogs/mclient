75c2ca72249a2e786144037abddf2913cf0d06bb
skl.progs <skl.progs@gmail.com>
2025-06-09 00:16:02 +0300

    Update version

--- src/about/logic.py
+++ src/about/logic.py
@@ -9,7 +9,7 @@ class About:
     
     def __init__(self):
         self.product = 'MClient'
-        self.version = '7.1.6'
+        self.version = '7.1.7'
         self.curyear = 2025
         self.code = []
     
