090d5a83541aa99e95ae513c8e9d40c6953e4f9c
skl.progs <skl.progs@gmail.com>
2025-08-22 04:10:34 +0300

    plugins.fora.stardictx.tags: Port tag parser from plugins.multitrancom

--- src/plugins/fora/stardictx/tags.py
+++ src/plugins/fora/stardictx/tags.py
@@ -8,11 +8,11 @@ from skl_shared.message.controller import rep, Message
 from skl_shared.localize import _
 from skl_shared.table import Table
 
-from instance import Block
+from instance import Block, Tag
 
 
 ''' Tag patterns:
-    •  Dictionary titles:
+    •  Full dictionary titles:
          define them by the .ifo file, set the tag manually
          <dic></dic>
     •  Short dictionary titles:
@@ -21,9 +21,9 @@ from instance import Block
     •  Terms:
          (no tags at all, see "'cellist")
          <dtrn></dtrn>
-         <kref></kref> (in phrases)
     •  Comments:
          <co></co>
+         <kref></kref> ("See also" section items)
     •  Word forms:
          <k></k>
     •  Transcription:
@@ -32,238 +32,295 @@ from instance import Block
          # XDXF tag meaning grammar information about the word <gr></gr>
     '''
 
-# Full dictionary titles
-pdic = '<dic>'
-
-# Comments
-pcom = '<co>'
-
-# Word Forms
-pwf = '<k>'
-
-# Parts of speech
-psp = '<gr>'
-
-# Terms
-ptm = '<dtrn>'
-
-# Terms in the 'Phrases' section
-pph = '<kref>'
-
-# Transcription
-ptr = '<tr>'
-
-useful_tags = [pdic, pcom, ptr, pwf, ptm, pph, psp]
-
+useful_tags = ['<dic>', '<co>', '<tr>', '<k>', '<dtrn>', '<kref>', '<gr>']
 
 
 class AnalyzeTag:
 
-    def __init__(self, tag):
-        self.fragm = ''
-        self.fragms = []
-        self.blocks = []
-        self.block = Block()
-        self.tag = tag
+    def __init__(self, fragm):
+        self.set_values()
+        self.fragm = fragm
+    
+    def set_values(self):
+        self.Success = True
+        self.tag = Tag()
+        self.cur_row = 0
+        self.cur_cell = 0
+    
+    def check(self):
+        f = '[MClient] plugins.fora.stardictx.tags.AnalyzeTag.check'
+        if not self.fragm:
+            self.Success = False
+            rep.empty(f)
+    
+    def _set_name(self):
+        # Do this before setting a URL
+        self.tag.name = self.tag.text
+        if self.tag.name.startswith('<'):
+            self.tag.name = self.tag.name[1:]
+        pos = self.tag.name.find(' ')
+        if pos > -1:
+            self.tag.name = self.tag.name[:pos]
+        self.tag.name = self.tag.name.lower()
+    
+    def _set_text(self):
+        f = '[MClient] plugins.fora.stardictx.tags.Tags._set_text'
+        self.tag.text = self.fragm
+        if self.tag.text.startswith('</'):
+            self.tag.text = self.tag.text[2:]
+        elif self.tag.text.startswith('<'):
+            self.tag.text = self.tag.text[1:]
+        else:
+            mes = _('Pattern "{}" is not a tag!').format(self.tag.text)
+            Message(f, mes).show_warning()
+        if self.tag.text.endswith('>'):
+            self.tag.text = self.tag.text[:-1]
+        else:
+            mes = _('Pattern "{}" is not a tag!').format(self.tag.text)
+            Message(f, mes).show_warning()
 
-    def set_dic(self):
-        if pdic in self.fragm:
-            self.block.type = 'subj'
+    def _is_tag(self):
+        if self.fragm.startswith('<') and self.fragm.endswith('>'):
+            return True
+    
+    def _is_term(self):
+        return 'dtrn' in self.tag.text
+    
+    def _is_subj(self):
+        return self.tag.name == 'dic'
+    
+    def _is_comment(self):
+        return self.tag.name in ('co', 'kref')
+    
+    def _is_wform(self):
+        return self.tag.name == 'k'
+    
+    def _is_transc(self):
+        return self.tag.name == 'tr'
+    
+    def _is_speech(self):
+        return self.tag.text == 'gr'
+    
+    def _set_type(self):
+        if self._is_term():
+            self.tag.type = 'term'
+        elif self._is_comment():
+            self.tag.type = 'comment'
+        elif self._is_subj():
+            self.tag.type = 'subj'
+        elif self._is_wform():
+            self.tag.type = 'wform'
+        elif self._is_speech():
+            self.tag.type = 'speech'
+        elif self._is_transc():
+            self.tag.type = 'transc'
+        else:
+            self.tag.type = 'invalid'
+    
+    def _set_close(self):
+        if self.fragm.startswith('</'):
+            self.tag.Close = True
+    
+    def set_attr(self):
+        f = '[MClient] plugins.fora.stardictx.tags.AnalyzeTag.set_attr'
+        if not self.Success:
+            rep.cancel(f)
+            return
+        if self._is_tag():
+            self._set_close()
+            self._set_text()
+            self._set_name()
+            self._set_type()
+        else:
+            self.tag.type = 'text'
+            self.tag.text = self.fragm
     
     def run(self):
-        f = '[MClient] plugins.fora.stardictx.tags.AnalyzeTag.run'
-        self.split()
-        self.fragms = [fragm for fragm in self.fragms if fragm.strip()]
-        for self.fragm in self.fragms:
-            if not self.fragm.startswith('<'):
-                self.run_plain()
-                continue
-            if not self.is_useful():
-                rep.wrong_input(f, self.fragm, False)
-                self.block.type = 'invalid'
-                continue
-            self.block.type = ''
-            self.set_phrases()
-            # Phrases and word forms have conflicting tags
-            # We check 'type' to speed up
-            if not self.block.type:
-                self.set_wform()
-            if not self.block.type:
-                self.set_dic()
-            if not self.block.type:
-                self.set_term()
-            if not self.block.type:
-                self.set_speech()
-            if not self.block.type:
-                self.set_comment()
-            if not self.block.type:
-                self.set_transc()
-        return self.blocks
-
-    def is_useful(self):
-        for tag in useful_tags:
-            if tag in self.fragm:
-                return True
-
-    def run_plain(self):
-        self.block.text = self.fragm
-        ''' #NOTE: The analysis must be reset after '</', otherwise, plain text
-            following it will be marked as 'invalid' rather than 'comment'.
-        '''
-        if self.block.type != 'invalid':
-            self.blocks.append(copy.copy(self.block))
-
-    def split(self):
-        ''' Use custom split because we need to preserve delimeters (cannot
-            distinguish tags and contents otherwise).
-        '''
-        tmp = ''
-        for sym in self.tag:
-            if sym == '>':
-                tmp += sym
-                self.fragms.append(tmp)
-                tmp = ''
-            elif sym == '<':
-                if tmp:
-                    self.fragms.append(tmp)
-                tmp = sym
-            else:
-                tmp += sym
-        if tmp:
-            self.fragms.append(tmp)
-
-    def set_comment(self):
-        if self.fragm.startswith(pcom):
-            self.block.type = 'comment'
-
-    def set_wform(self):
-        if pwf in self.fragm:
-            self.block.type = 'wform'
-
-    def set_phrases(self):
-        if pph in self.fragm:
-            self.block.type = 'phrase'
-
-    def set_term(self):
-        if ptm in self.fragm:
-            self.block.type = 'term'
-
-    def set_transc(self):
-        if ptr in self.fragm:
-            self.block.type = 'transc'
-
-    def set_speech(self):
-        if psp in self.fragm:
-            self.block.type = 'speech'
+        self.check()
+        self.set_attr()
+        return self.tag
 
 
 
 class Tags:
-
-    def __init__(self, text, Debug=False, maxrow=20, maxrows=1000):
-        if not text:
-            text = ''
-        self.text = text
+    
+    def __init__(self, text):
+        self.set_values()
+        self.code = text
+    
+    def set_values(self):
+        self.Success = True
         self.abbr = {}
         self.blocks = []
-        self.Debug = Debug
-        self.maxrow = maxrow
-        self.maxrows = maxrows
+        self.fragms = []
         self.tags = []
+        self.open = []
     
-    def set_tags(self):
-        ''' Split the text by closing tags. To speed up, we remove closing tags
-            right away.
-        '''
-        Ignore = False
-        tmp = ''
-        for i in range(len(self.text)):
-            if self.text[i] == '\n':
-                if tmp:
-                    self.tags.append(tmp)
-                    tmp = ''
-            elif self.text[i] == '<':
-                if i < len(self.text) - 1 and self.text[i+1] == '/':
-                    Ignore = True
-                    if tmp:
-                        self.tags.append(tmp)
-                        tmp = ''
-                else:
-                    tmp += self.text[i]
-            elif self.text[i] == '>':
-                if Ignore:
-                    Ignore = False
-                else:
-                    tmp += self.text[i]
-            elif not Ignore:
-                tmp += self.text[i]
-        # Should be needed only for broken tags
-        if tmp:
-            self.tags.append(tmp)
-
-    def debug_tags(self):
-        f = '[MClient] plugins.fora.stardictx.tags.Tags.debug_tags'
-        mes = []
-        for i in range(len(self.tags)):
-            mes.append(f'{i}:{self.tags[i]}')
-        mes.insert(0, f'{f}:')
-        return '\n'.join(mes)
-
-    def debug_blocks (self):
-        f = '[MClient] plugins.fora.stardictx.tags.Tags.debug_blocks'
-        headers = ('NO', 'CELLNO', 'TYPE', 'TEXT', 'URL')
-        rows = []
-        for i in range(len(self.blocks)):
-            rows.append([i+1, self.blocks[i].cellno, self.blocks[i].type
-                       ,self.blocks[i].text, self.blocks[i].url])
-        mes = Table(headers=headers, iterable=rows, maxrow=self.maxrow
-                   ,maxrows=self.maxrows, Transpose=True).run()
-        return f + ':\n' + mes
-
-    def debug(self):
-        report = [self.debug_tags(), self.debug_blocks()]
-        report = [item for item in report if item]
-        return '\n\n'.join(report)
-
+    def _is_trash(self, tag):
+        for subtag in tag.inherent:
+            if subtag.type == 'trash':
+                return True
+    
+    def _close(self, name):
+        i = len(self.open) - 1
+        while i >= 0:
+            if self.open[i].name == name:
+                del self.open[i]
+                return True
+            i -= 1
+    
+    def set_inherent(self):
+        f = '[MClient] plugins.fora.stardictx.tags.Tags.set_inherent'
+        if not self.Success:
+            rep.cancel(f)
+            return
+        for tag in self.tags:
+            if tag.Close:
+                self._close(tag.name)
+            elif tag.type == 'text':
+                tag.inherent = list(self.open)
+            elif tag.type:
+                self.open.append(tag)
+    
+    def set_nos(self):
+        f = '[MClient] plugins.fora.stardictx.tags.Tags.set_nos'
+        if not self.Success:
+            rep.cancel(f)
+            return
+        curcell = -1
+        for tag in self.tags:
+            if not tag.Close:
+                if tag.name in ('dic', 'dtrn', 'tr'):
+                    curcell += 1
+            tag.cellno = curcell
+    
+    def _debug_blocks(self):
+        nos = [i + 1 for i in range(len(self.blocks))]
+        types = [block.type for block in self.blocks]
+        texts = [f'"{block.text}"' for block in self.blocks]
+        urls = [f'"{block.url}"' for block in self.blocks]
+        subjs = [f'"{block.subj}"' for block in self.blocks]
+        subjfs = [f'"{block.subjf}"' for block in self.blocks]
+        cellnos = [block.cellno for block in self.blocks]
+        iterable = [nos, types, texts, urls, subjs, subjfs, cellnos]
+        headers = (_('#'), _('TYPE'), _('TEXT'), 'URL', 'SUBJ', 'SUBJF'
+                  ,_('CELL #'))
+        # 10'' monitor: 20 symbols per a column
+        # 23'' monitor: 50 symbols per a column
+        mes = Table(iterable = iterable, headers = headers, maxrow = 50
+                   ,maxrows = 1000).run()
+        return _('Blocks:') + '\n' + mes
+    
     def set_blocks(self):
-        cellno = -1
-        for tag in self.tags:
-            blocks = AnalyzeTag(tag).run()
-            if not blocks:
-                continue
-            cellno += 1
-            blocks[0].cellno = cellno
-            i = 1
-            while i < len(blocks):
-                blocks[i].cellno = cellno
-                i += 1
-            self.blocks += blocks
-
-    def split_lines(self):
-        tags = []
-        for tag in self.tags:
-            tag = tag.splitlines()
-            tag = [item.strip() for item in tag if item.strip()]
-            for i in range(len(tag)):
-                if not '<' in tag[i] and not '>' in tag[i]:
-                    tag[i] = f'<dtrn>{tag[i]}</dtrn>'
-            tags += tag
-        self.tags = tags
+        f = '[MClient] plugins.fora.stardictx.tags.Tags.set_blocks'
+        if not self.Success:
+            rep.cancel(f)
+            return
+        tags = [tag for tag in self.tags if tag.type == 'text' \
+               and not self._is_trash(tag)]
+        for tag in tags:
+            block = Block()
+            for subtag in tag.inherent:
+                if subtag.type == 'url':
+                    block.url = subtag.url
+                    block.subjf = subtag.subjf
+                else:
+                    block.type = subtag.type
+            block.text = tag.text
+            block.cellno = tag.cellno
+            block.text = html.unescape(block.text)
+            if block.type in ('subj', 'phsubj'):
+                block.subj = block.text
+            self.blocks.append(block)
     
-    def unescape(self):
-        ''' Convert HTM entities to a human-readable format, for example,
-            '&copy;' -> '©'.
+    def assign(self):
+        f = '[MClient] plugins.fora.stardictx.tags.Tags.assign'
+        if not self.Success:
+            rep.cancel(f)
+            return
+        for fragm in self.fragms:
+            self.tags.append(AnalyzeTag(fragm).run())
+    
+    def _debug_code(self):
+        return _('Code:') + '\n' + '"{}"'.format(self.code)
+    
+    def _debug_fragms(self):
+        mes = []
+        for i in range(len(self.fragms)):
+            mes.append(f'{i+1}: "{self.fragms[i]}"')
+        return _('Fragments:') + '\n' + '\n'.join(mes)
+    
+    def _debug_tags(self):
+        nos = [i + 1 for i in range(len(self.tags))]
+        closes = [f'{tag.Close}' for tag in self.tags]
+        names = [f'"{tag.name}"' for tag in self.tags]
+        types = [f'"{tag.type}"' for tag in self.tags]
+        texts = [f'"{tag.text}"' for tag in self.tags]
+        urls = [f'"{tag.url}"' for tag in self.tags]
+        subjfs = [f'"{tag.subjf}"' for tag in self.tags]
+        cellnos = [f'{tag.cellno}' for tag in self.tags]
+        inherent = []
+        for tag in self.tags:
+            subtags = []
+            for subtag in tag.inherent:
+                subtags.append(subtag.name)
+            subtags = ', '.join(subtags)
+            inherent.append(subtags)
+        iterable = [nos, closes, names, types, texts, urls, subjfs, inherent
+                   ,cellnos]
+        headers = (_('#'), _('CLOSING'), _('NAME'), _('TYPE'), _('TEXT'), 'URL'
+                  ,'DICF', _('OPEN'), _('CELL'))
+        # 10'' monitor: 13 symbols per a column
+        # 23'' monitor: 30 symbols per a column
+        mes = Table(iterable = iterable, headers = headers, maxrow = 70
+                   ,maxrows = 1000).run()
+        return _('Tags:') + '\n' + mes
+    
+    def debug(self):
+        f = '[MClient] plugins.fora.stardictx.tags.Tags.debug'
+        if not self.Success:
+            rep.cancel(f)
+            return ''
         '''
-        f = '[MClient] plugins.fora.stardictx.tags.Tags.unescape'
-        for block in self.blocks:
-            try:
-                block.text = html.unescape(block.text)
-            except Exception as e:
-                rep.failed(f, e)        
+        mes = [self._debug_code(), self._debug_fragms(), self._debug_tags()
+              ,self._debug_blocks()]
+        '''
+        mes = [self._debug_tags(), self._debug_blocks()]
+        return '\n\n'.join(mes)
+    
+    def check(self):
+        f = '[MClient] plugins.fora.stardictx.tags.Tags.check'
+        if not self.code:
+            # Avoid None on output
+            self.code = ''
+            self.Success = False
+            rep.empty(f)
+    
+    def split(self):
+        f = '[MClient] plugins.fora.stardictx.tags.Tags.split'
+        if not self.Success:
+            rep.cancel(f)
+            return
+        fragm = ''
+        for sym in list(self.code):
+            if sym == '<':
+                if fragm:
+                    self.fragms.append(fragm)
+                fragm = sym
+            elif sym == '>':
+                fragm += sym
+                self.fragms.append(fragm)
+                fragm = ''
+            else:
+                fragm += sym
+        if fragm:
+            self.fragms.append(fragm)
     
     def run(self):
-        self.set_tags()
-        self.split_lines()
+        self.check()
+        self.split()
+        self.assign()
+        self.set_inherent()
+        self.set_nos()
         self.set_blocks()
-        self.unescape()
         return self.blocks
