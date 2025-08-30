6a2fc8e8f75d02d6bfc9abc6ca97029f31efde62
skl.progs <skl.progs@gmail.com>
2025-08-30 13:11:01 +0300

    plugins.stardict.elems: Remove heuristics based on old CleanUp

--- src/plugins/stardict/elems.py
+++ src/plugins/stardict/elems.py
@@ -11,8 +11,6 @@ from skl_shared.logic import Text, punc_array, ru_alphabet, lat_alphabet
 
 from instance import Block, Cell
 
-#TODO: Comment out heuristics through this module
-HEURISTICS = False
 SPEECH_ABBR = ('гл.', 'нареч.', 'нар.', 'прил.', 'сокр.', 'сущ.')
 SUBJ_ABBR = ('амер.', 'вчт.', 'геогр.', 'карт.', 'мор.', 'разг.', 'уст.', 'хир.', 'эл.')
 #TODO: read from file
@@ -113,10 +111,6 @@ class Phrases:
             i += 1
     
     def run(self):
-        f = '[MClient] plugins.stardict.elems.Phrases.run'
-        if not HEURISTICS:
-            rep.lazy(f)
-            return self.blocks
         self.replace_seps()
         self.split()
         self.remove_trash()
@@ -198,34 +192,6 @@ class Elems:
     def set_urls(self):
         for cell in self.cells:
             cell.url = self._get_url(cell)
-    
-    def unite_brackets(self):
-        f = '[MClient] plugins.stardict.elems.Elems.unite_brackets'
-        if not HEURISTICS:
-            rep.lazy(f)
-            return
-        ''' Combine a cell with a preceding or following bracket such that the
-            user would not see '()' when the cell is ignored/blocked.
-        '''
-        f = '[MClient] plugins.stardict.elems.Elems.unite_brackets'
-        count = 0
-        for cell in self.cells:
-            i = 2
-            while i < len(cell.blocks):
-                if cell.blocks[i-2].text.strip() == '(' \
-                and cell.blocks[i].text.strip() == ')':
-                    count += 1
-                    ''' Add brackets to text of a cell (usually of the 'user'
-                        type), not vice versa, to preserve its type.
-                    '''
-                    cell.blocks[i-1].text = cell.blocks[i-2].text \
-                                          + cell.blocks[i-1].text \
-                                          + cell.blocks[i].text
-                    del cell.blocks[i]
-                    del cell.blocks[i-2]
-                    i -= 2
-                i += 1
-        rep.matches(f, count)
     
     def set_text(self):
         for cell in self.cells:
@@ -363,45 +329,22 @@ class Elems:
             if block.text in SUBJ_ABBR or block.text in SUBJ:
                 block.type = 'subj'
     
-    def set_synonyms(self):
-        f = '[MClient] plugins.stardict.elems.Elems.set_synonyms'
-        if not HEURISTICS:
-            rep.lazy(f)
-            return
-        i = 1
-        while i < len(self.blocks):
-            if 'Syn :' in self.blocks[i].text:
-                self.blocks[i].text = self.blocks[i].text.replace('Syn :', _('Synonyms:'))
-                self.blocks[i].type = 'comment'
-                self.blocks[i].cellno = self.blocks[i-1].cellno
-            i += 1
-    
     def run(self):
         f = '[MClient] plugins.stardict.elems.Elems.run'
         if not self.Success:
             rep.cancel(f)
             return []
-        self.delete_straight_line()
         self.set_subjects()
         self.set_speech()
-        self.set_synonyms()
         self.set_fixed_blocks()
-        self.run_comments()
-        ''' These 2 procedures should not be combined (otherwise, corrections
-            will have the same color as comments)
-        '''
-        self.unite_comments()
-        self.set_com_same()
-        self.add_space()
         self.expand_dic()
-        self.set_phrases()
-        self.move_phrases()
-        self.set_phsubj_name()
-        self.set_phsubj()
-        self.blocks = Phrases(self.blocks).run()
+        #self.set_phrases()
+        #self.move_phrases()
+        #self.set_phsubj_name()
+        #self.set_phsubj()
+        #self.blocks = Phrases(self.blocks).run()
         self.set_cells()
         self.set_urls()
-        self.unite_brackets()
         self.set_text()
         self.set_fixed_blocks()
         self.set_fixed_cells()
@@ -467,170 +410,6 @@ class Elems:
                                ,texts, urls)
                    ,maxrow = maxrow, maxrows = maxrows).run()
         return f'{f}:\n{mes}'
-        
-    def unite_comments(self):
-        f = '[MClient] plugins.stardict.elems.Elems.unite_comments'
-        if not HEURISTICS:
-            rep.lazy(f)
-            return
-        i = 1
-        while i < len(self.blocks):
-            if self.blocks[i].type == 'comment' \
-            and self.blocks[i-1].cellno == self.blocks[i].cellno:
-                if i > 0 and self.blocks[i-1].type == 'comment':
-                    self.blocks[i-1].text = List(lst1 = [self.blocks[i-1].text
-                                                ,self.blocks[i].text]).space_items()
-                    del self.blocks[i]
-                    i -= 1
-            i += 1
-            
-    def delete_straight_line(self):
-        f = '[MClient] plugins.stardict.elems.Elems.delete_straight_line'
-        if not HEURISTICS:
-            rep.lazy(f)
-            return
-        self.blocks = [block for block in self.blocks if block.text.strip() != '|']
-    
-    def run_comments(self):
-        f = '[MClient] plugins.stardict.elems.Elems.run_comments'
-        if not HEURISTICS:
-            rep.lazy(f)
-            return
-        i = 0
-        cellno = -1
-        while i < len(self.blocks):
-            if self.blocks[i].type in ('comment', 'correction'):
-                text_str = self.blocks[i].text.strip()
-                ''' Delete comments that are just ';' or ',' (we don't need
-                    them, we have a table view). We delete instead of
-                    assigning Block attribute because we may need to unblock
-                    blocked subjects later.
-                '''
-                if text_str == ';' or text_str == ',':
-                    del self.blocks[i]
-                    i -= 1
-                elif self.blocks[i].cellno == cellno:
-                    # For the following cases: "23 фраз в 9 тематиках"
-                    if i > 0 and self.blocks[i-1].type == 'phrase':
-                        self.blocks[i].cellno = cellno
-                    # Move the comment to the preceding cell
-                    if text_str.startswith(',') or text_str.startswith(';') \
-                    or text_str.startswith('(') or text_str.startswith(')') \
-                    or text_str.startswith('|'):
-                        self.blocks[i].cellno = cellno
-                        # Mark the next block as a start of a new cell
-                        if i < len(self.blocks) - 1 and self.blocks[i+1].type \
-                        not in ('comment', 'correction'):
-                            cellno += 1
-                            self.blocks[i+1].cellno = cellno
-            i += 1
-            
-    def set_com_same(self):
-        f = '[MClient] plugins.stardict.elems.Elems.set_com_same'
-        if not HEURISTICS:
-            rep.lazy(f)
-            return
-        ''' Sometimes sources do not provide sufficient information on
-            SAMECELL blocks, and the tag parser cannot handle sequences
-            such as 'any type (not same) -> comment (not same) ->
-            any type (not same)'.
-            Rules:
-            1) (Should be always correct)
-                'i >= 0 -> correction (not same)
-                    =>
-                'i >= 0 -> correction (same)
-            2) (Preferable)
-                'term (not same) -> comment (not same) -> any type
-                (not same)'
-                    =>
-                'term (not same) -> comment (same) -> any type
-                (not same)'
-            3) (Generally correct before removing fixed columns)
-                'dic/wform/speech/transc -> comment (not same) -> term
-                (not same)'
-                    =>
-                'dic/wform/speech/transc -> comment (not same) -> term
-                (same)'
-            4) (By guess, check only after ##2&3)
-                'any type (same) -> comment (not same) -> any type
-                (not same)'
-                    =>
-                'any type (same) -> comment (same) -> any type
-                (not same)'
-            5) (Always correct)
-                'any type -> comment/correction (not same) -> END'
-                    =>
-                'any type -> comment/correction (same) -> END'
-            6) (Do this in the end of the loop + Readability improvement
-               ("в 42 тематиках"))
-                'any type (not same) -> comment (not same) -> any type
-                (not same)'
-                    =>
-                'any type (not same) -> comment (same) -> any type
-                (not same)'
-        '''
-        if not self.blocks:
-            return
-        i = 3
-        while i < len(self.blocks):
-            cond1 = self.blocks[i].type == 'correction'
-            cond2 = False
-            if self.blocks[i-1].cellno != self.blocks[i].cellno:
-                cond2 = True
-            cond3 = self.blocks[i-1].type == 'comment' \
-                    and self.blocks[i-2].cellno == self.blocks[i-1].cellno
-            cond4 = self.blocks[i-2].type == 'term' \
-                    and self.blocks[i-3].cellno == self.blocks[i-2].cellno
-            cond5 = self.blocks[i-3].cellno == self.blocks[i-2].cellno
-            cond6 = self.blocks[i].type == 'term'
-            cond7a = self.blocks[i-2].type == 'subj'
-            cond7b = self.blocks[i-2].type == 'wform'
-            cond7c = self.blocks[i-2].type == 'speech'
-            cond7d = self.blocks[i-2].type == 'transc'
-            cond7 = cond7a or cond7b or cond7c or cond7d
-            # Rule 1
-            if cond1 and cond2:
-                self.blocks[i].same = 1
-            # Rule 2
-            elif cond4 and cond3 and cond2:
-                self.blocks[i-1].same = 1
-            # Rule 3
-            elif cond7 and cond3 and cond6 and cond2:
-                self.blocks[i].same = 1
-            # Rule 4:
-            elif cond5 and cond3 and cond2:
-                self.blocks[i-1].same = 1
-            # Rule 6:
-            elif cond5 and cond3 and cond2:
-                self.blocks[i-1].same = 1
-            i += 1
-        # Rule 5
-        # After exiting the loop, the last block
-        i = len(self.blocks) - 1
-        cond1 = self.blocks[i].type in ('comment', 'correction')
-        cond2 = self.blocks[i-1].cellno == self.blocks[i].cellno
-        if cond1 and cond2:
-            self.blocks[i].cellno = self.blocks[i-1].cellno
-    
-    def add_space(self):
-        f = '[MClient] plugins.stardict.elems.Elems.add_space'
-        if not HEURISTICS:
-            rep.lazy(f)
-            return
-        i = 1
-        while i < len(self.blocks):
-            if self.blocks[i-1].cellno == self.blocks[i].cellno:
-                cond = False
-                if i > 0:
-                    if self.blocks[i-1].text[-1] in ['(', '[', '{']:
-                        cond = True
-                if self.blocks[i].text \
-                  and not self.blocks[i].text[0].isspace() \
-                  and not self.blocks[i].text[0] in punc_array \
-                  and not self.blocks[i].text[0] in [')', ']', '}'] \
-                  and not cond:
-                    self.blocks[i].text = ' ' + self.blocks[i].text
-            i += 1
 
     def _has_sep(self, fragm):
         for sep in ('~', '≈', '*'):
