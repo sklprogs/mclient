3236b00f56a922cbb8bd83d9655f4559e138f704
skl.progs <skl.progs@gmail.com>
2025-10-05 01:29:24 +0300

    Remove plugins.stardict.run.Plugin.is_combined

--- src/plugins/stardict/run.py
+++ src/plugins/stardict/run.py
@@ -132,10 +132,6 @@ class Plugin:
         # This is needed only for compliance with a general method
         return(_('Any'),)
     
-    def is_combined(self):
-        # Whether or not the plugin is actually a wrapper over other plugins
-        return False
-    
     def count_valid(self):
         return len(gt.ALL_DICS.get_valid())
     
