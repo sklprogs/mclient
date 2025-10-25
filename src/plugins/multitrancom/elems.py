718ba371f5e86801a9c3771f5a4ef1fde4db9fe3
skl.progs <skl.progs@gmail.com>
2025-10-26 02:40:07 +0300

    plugins.multitrancom.elems: Do not create cells

--- src/plugins/multitrancom/elems.py
+++ src/plugins/multitrancom/elems.py
@@ -434,38 +434,17 @@ class Suggestions:
 class Elems:
     
     def __init__(self, blocks):
-        self.cells = []
         self.art_subj = {}
-        self.fixed_urls = {'subj':{}, 'wform':{}, 'phsubj':{}}
         self.Parallel = False
         self.Separate = False
         self.blocks = blocks
     
-    def save_urls(self):
-        for cell in self.cells:
-            if not cell.fixed_block:
-                continue
-            if cell.fixed_block.type == 'subj':
-                self.fixed_urls[cell.fixed_block.type][cell.fixed_block.subj] = cell.url
-                self.fixed_urls[cell.fixed_block.type][cell.fixed_block.subjf] = cell.url
-            elif cell.fixed_block.type in ('phsubj', 'wform') and cell.url:
-                self.fixed_urls[cell.fixed_block.type][cell.text] = cell.url
-    
     def _is_block_fixed(self, block):
         return block.type in ('subj', 'wform', 'speech', 'transc', 'phsubj')
-    
-    def _get_fixed_block(self, cell):
-        for block in cell.blocks:
-            if block.Fixed:
-                return block
     
     def set_fixed_blocks(self):
         for block in self.blocks:
             block.Fixed = self._is_block_fixed(block)
-    
-    def set_fixed_cells(self):
-        for cell in self.cells:
-            cell.fixed_block = self._get_fixed_block(cell)
     
     def run_phcount(self):
         f = '[MClient] plugins.multitrancom.elems.Elems.run_phcount'
@@ -479,40 +458,8 @@ class Elems:
             i += 1
         rep.matches(f, count)
     
-    def set_cells(self):
-        f = '[MClient] plugins.multitrancom.elems.Elems.set_cells'
-        if not self.blocks:
-            rep.empty(f)
-            return
-        if len(self.blocks) < 2:
-            rep.condition(f, f'{len(self.blocks)} >= 2')
-            return
-        cell = ic.Cell()
-        cell.blocks.append(self.blocks[0])
-        i = 1
-        while i < len(self.blocks):
-            if self.blocks[i-1].cellno == self.blocks[i].cellno:
-                cell.blocks.append(self.blocks[i])
-            else:
-                if cell.blocks:
-                    self.cells.append(cell)
-                cell = ic.Cell()
-                cell.blocks.append(self.blocks[i])
-            i += 1
-        if cell.blocks:
-            self.cells.append(cell)
-    
-    def renumber(self):
-        for i in range(len(self.cells)):
-            self.cells[i].no = i
-    
-    def debug(self):
-        report = [self._debug_blocks(), self._debug_cells()]
-        report = [item for item in report if item]
-        return '\n\n'.join(report)
-    
-    def _debug_blocks(self, maxrow=30, maxrows=0):
-        f = '[MClient] plugins.multitrancom.elems.Elems._debug_blocks'
+    def debug(self, maxrow=30, maxrows=0):
+        f = '[MClient] plugins.multitrancom.elems.Elems.debug'
         headers = (_('CELL #'), _('TYPES'), _('TEXT'), 'SUBJ', 'SUBJF', 'URL')
         nos = []
         types = []
@@ -532,72 +479,11 @@ class Elems:
                    ,maxrow = maxrow, maxrows = maxrows).run()
         return f'{f}:\n{mes}'
     
-    def _debug_cells(self, maxrow=70, maxrows=0):
-        f = '[MClient] plugins.multitrancom.elems.Elems._debug_cells'
-        headers = ('SUBJ', 'WFORM', 'SPEECH', 'TRANSC', _('ROW #'), _('CELL #')
-                  ,_('TYPES'), _('TEXT'), 'URL')
-        subj = []
-        wform = []
-        speech = []
-        transc = []
-        rownos = []
-        nos = []
-        types = []
-        texts = []
-        urls = []
-        for cell in self.cells:
-            subj.append(cell.subj)
-            wform.append(cell.wform)
-            speech.append(cell.speech)
-            transc.append(cell.transc)
-            rownos.append(cell.rowno)
-            nos.append(cell.no)
-            texts.append(f'"{cell.text}"')
-            cell_types = [block.type for block in cell.blocks]
-            types.append(', '.join(cell_types))
-            urls.append(cell.url)
-        mes = Table(headers = headers
-                   ,iterable = (subj, wform, speech, transc, rownos, nos, types
-                               ,texts, urls)
-                   ,maxrow = maxrow, maxrows = maxrows).run()
-        return f'{f}:\n{mes}'
-    
-    def set_text(self):
-        for cell in self.cells:
-            fragms = [block.text for block in cell.blocks]
-            cell.text = List(fragms).space_items().strip()
-            # 'phsubj' text may have multiple spaces for some reason
-            cell.text = Text(cell.text).delete_duplicate_spaces()
-    
     def delete_semi(self):
         f = '[MClient] plugins.multitrancom.elems.Elems.delete_semi'
         old_len = len(self.blocks)
         self.blocks = [block for block in self.blocks if block.text != '; ']
         rep.matches(f, old_len-len(self.blocks))
-    
-    def unite_brackets(self):
-        ''' Combine a cell with a preceding or following bracket such that the
-            user would not see '()' when the cell is ignored/blocked.
-        '''
-        f = '[MClient] plugins.multitrancom.elems.Elems.unite_brackets'
-        count = 0
-        for cell in self.cells:
-            i = 2
-            while i < len(cell.blocks):
-                if cell.blocks[i-2].text.strip() == '(' \
-                and cell.blocks[i].text.strip() == ')':
-                    count += 1
-                    ''' Add brackets to text of a cell (usually of the 'user'
-                        type), not vice versa, to preserve its type.
-                    '''
-                    cell.blocks[i-1].text = cell.blocks[i-2].text \
-                                          + cell.blocks[i-1].text \
-                                          + cell.blocks[i].text
-                    del cell.blocks[i]
-                    del cell.blocks[i-2]
-                    i -= 2
-                i += 1
-        rep.matches(f, count)
     
     def separate_fixed(self):
         f = '[MClient] plugins.multitrancom.elems.Elems.separate_fixed'
@@ -658,17 +544,6 @@ class Elems:
             i += 1
         rep.matches(f, count)
     
-    def _get_url(self, cell):
-        #TODO: Do we need to support several URLs in one cell?
-        for block in cell.blocks:
-            if block.url:
-                return block.url
-        return ''
-    
-    def set_urls(self):
-        for cell in self.cells:
-            cell.url = self._get_url(cell)
-    
     def set_see_also(self):
         # Example: "beg the question"
         i = 1
@@ -683,125 +558,12 @@ class Elems:
                     self.blocks[i-1].url = self.blocks[i].url
             i += 1
     
-    def _get_last_subj(self):
-        for cell in self.cells[::-1]:
-            if cell.fixed_block and cell.fixed_block.type in ('subj', 'phsubj'):
-                return cell.text
-    
-    def _get_last_wform(self):
-        for cell in self.cells[::-1]:
-            if cell.fixed_block and cell.fixed_block.type == 'wform':
-                return cell.text
-    
-    def _get_last_speech(self):
-        for cell in self.cells[::-1]:
-            if cell.fixed_block and cell.fixed_block.type == 'speech':
-                return cell.text
-    
-    def _get_last_transc(self):
-        for cell in self.cells[::-1]:
-            if cell.fixed_block and cell.fixed_block.type == 'transc':
-                return cell.text
-    
-    def _get_prev_subj(self, i):
-        while i >= 0:
-            if self.cells[i].fixed_block \
-            and self.cells[i].fixed_block.type in ('subj', 'phsubj'):
-                return self.cells[i].text
-            i -= 1
-        return ''
-    
-    def _get_prev_wform(self, i):
-        while i >= 0:
-            if self.cells[i].fixed_block \
-            and self.cells[i].fixed_block.type == 'wform':
-                return self.cells[i].text
-            i -= 1
-        return ''
-    
-    def _get_prev_speech(self, i):
-        while i >= 0:
-            if self.cells[i].fixed_block \
-            and self.cells[i].fixed_block.type == 'speech':
-                return self.cells[i].text
-            i -= 1
-        return ''
-    
-    def _get_prev_transc(self, i):
-        while i >= 0:
-            if self.cells[i].fixed_block:
-                if self.cells[i].fixed_block.type == 'wform':
-                    return ''
-                if self.cells[i].fixed_block.type == 'transc':
-                    return self.cells[i].text
-            i -= 1
-        return ''
-    
-    def fill_fixed(self):
-        subj = self._get_last_subj()
-        wform = self._get_last_wform()
-        transc = self._get_last_transc()
-        speech = self._get_last_speech()
-        i = len(self.cells) - 1
-        while i >= 0:
-            if not self.cells[i].fixed_block:
-                subj = self._get_prev_subj(i)
-                wform = self._get_prev_wform(i)
-                speech = self._get_prev_speech(i)
-                transc = self._get_prev_transc(i)
-            self.cells[i].subj = subj
-            self.cells[i].wform = wform
-            self.cells[i].speech = speech
-            self.cells[i].transc = transc
-            i -= 1
-    
-    def delete_fixed(self):
-        f = '[MClient] plugins.multitrancom.elems.Elems.delete_fixed'
-        count = 0
-        i = 0
-        while i < len(self.cells):
-            if self.cells[i].fixed_block:
-                count += 1
-                del self.cells[i]
-                i -= 1
-            i += 1
-        rep.matches(f, count)
-    
     def strip_blocks(self):
         # Needed for 'phsubj' and such 'wform' as 'English Thesaurus'
         for block in self.blocks:
             if not block.Fixed:
                 continue
             block.text = block.text.strip()
-    
-    def rename_phsubj(self):
-        for cell in self.cells:
-            if cell.fixed_block and cell.fixed_block.type == 'phsubj':
-                match = re.search(r'(\d+)', cell.text)
-                if match:
-                    title = _('Phrases ({})').format(match.group(1))
-                    # 'fill_fixed' is block-oriented
-                    cell.text = cell.fixed_block.text = cell.fixed_block.subj \
-                              = cell.fixed_block.subjf = title
-                    # There should be only one 'phsubj'
-                    return
-    
-    def set_row_nos(self):
-        # Run this before deleting fixed types
-        f = '[MClient] plugins.multitrancom.elems.Elems.set_row_nos'
-        count = 0
-        if self.cells:
-            count += 1
-            self.cells[0].rowno = 0
-        rowno = 0
-        i = 1
-        while i < len(self.cells):
-            if not self.cells[i-1].fixed_block and self.cells[i].fixed_block:
-                count += 1
-                rowno += 1
-            self.cells[i].rowno = rowno
-            i += 1
-        rep.matches(f, count)
     
     def set_art_subj(self):
         f = '[MClient] plugins.multitrancom.elems.Elems.set_art_subj'
@@ -890,16 +652,4 @@ class Elems:
         self.run_com_sp_com()
         self.unite_comments()
         self.renumber_by_type()
-        self.set_cells()
-        self.set_urls()
-        self.unite_brackets()
-        self.set_text()
-        self.set_fixed_cells()
-        self.rename_phsubj()
-        self.set_row_nos()
-        self.save_urls()
-        self.set_art_subj()
-        self.fill_fixed()
-        self.delete_fixed()
-        self.renumber()
-        return self.cells
+        return self.blocks
