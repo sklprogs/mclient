b36f30b13944dd60c68f0ab81981e00dd9e830e2
skl.progs <skl.progs@gmail.com>
2026-02-28 14:51:08 +0300

    tests.dump.dsl: Clean uo

--- src/tests/dump/dsl.py
+++ src/tests/dump/dsl.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 # -*- coding: UTF-8 -*-
 
-from skl_shared.localize import _
-from skl_shared.message.controller import Message, rep
-from cells import Elems
 from sources.dsl.get import ALL_DICS
 from sources.dsl.run import Source
 
@@ -18,7 +15,7 @@ class Dump(tests.dump.shared.Dump):
         super().__init__(*args, **kwargs)
     
     def run(self):
-        self.limit = 2
-        #return self.run_loops(1)
+        self.limit = 10
+        return self.run_loops(1)
         #return self.run_all()
-        return self.run_blocks()
+        #return self.run_blocks()
