fc314b1db5b3a4d8bad59e5747aba63e7a732d90
skl.progs <skl.progs@gmail.com>
2026-03-17 01:25:07 +0300

    font_limits.controller: Expand set_values. Style

--- src/font_limits/controller.py
+++ src/font_limits/controller.py
@@ -7,21 +7,14 @@ from font_limits.gui import FontLimits as guiFontLimits
 class FontLimits:
     
     def __init__(self, family, size, Bold=False, Italic=False):
-        self.set_values()
+        self.text = ''
+        self.font = None
         self.family = family
         self.size = size
         self.Bold = Bold
         self.Italic = Italic
         self.gui = guiFontLimits()
         self.set_font()
-    
-    def set_values(self):
-        self.family = 'Sans'
-        self.text = ''
-        self.font = None
-        self.size = 0
-        self.Bold = False
-        self.Italic = False
     
     def set_text(self, text):
         self.text = str(text)
@@ -32,13 +25,8 @@ class FontLimits:
             weight = 700
         else:
             weight = 400
-        self.font = self.gui.get_font(self.family
-                                     ,size = self.size
-                                     ,weight = weight
-                                     ,italic = self.Italic)
+        self.font = self.gui.get_font(self.family, size = self.size
+                                     ,weight = weight, italic = self.Italic)
     
     def get_space(self):
-        space = self.gui.get_space(self.text, self.font)
-        #mes = _('Space: {}').format(space)
-        #Message(f, mes).show_debug()
-        return space
+        return self.gui.get_space(self.text, self.font)
